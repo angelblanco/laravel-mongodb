0a34a9640092cd7045913951f140b6d83044d78b
angelblanco <angelblanco@users.noreply.github.com>
2020-11-15 23:36:09 +0000

    :zap: CI format code!

--- src/Query/Builder.php
+++ src/Query/Builder.php
@@ -1098,7 +1098,7 @@ class Builder extends BaseBuilder
             }
 
             // Convert to regular expression.
-            $regex = preg_replace('#(^|[^\\\])%#', '$1.*', preg_quote($value));
+            $regex = preg_replace("#(^|[^\\\])%#", '$1.*', preg_quote($value));
             $plain_value = $value;
 
             // Convert like to regular expression.
